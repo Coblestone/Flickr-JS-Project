d08f0a746f82a30f97ede56eaa766c4fd025c903
Antoine Drouard <antoine.drouard@etu.univ-nantes.fr>
2017-03-29 16:05:54 +0200

    img lié à la recherche

--- ressource/script/flickr.js
+++ ressource/script/flickr.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
-    $.ajax({
-    url:'http://api.flickr.com/services/feeds/photos_public.gne',
-    type:'GET',
-    dataType:'jsonp',
-    jsonp: 'jsoncallback', // a renseigner d'après la doc du service, par défaut callback
-    data:'tags=nantes&tagmode=any&format=json',
-    success:function(data){
-    $.each(data.items, function(i,item){
-                $("<img/>").attr("src", item.media.m).appendTo("#images");
-                if ( i == 6 ) return false; });
-              },
-    error: function(resultat,statut,erreur){
-    alert("erreur");},
-     });
+    $("#soumettre").on('click', function(event) {
+        $.ajax({
+            url:'http://api.flickr.com/services/feeds/photos_public.gne',
+            type:'GET',
+            dataType:'jsonp',
+            jsonp: 'jsoncallback', // a renseigner d'après la doc du service, par défaut callback
+            data:'tags= '+$('#commune').val()+'&tagmode=any&format=json',
+            success:function(data){
+
+                $.each(data.items, function(i,item){
+                    $("<img/>").attr("src", item.media.m).appendTo("#images");
+                    if ( i == 6 ) return false; 
+                });
+            },
+            error: function(resultat,statut,erreur){
+                alert("erreur");
+            },
+        });
+    });
 });
